386e548871ac8845f9734bfdcc76dbf6867987ce
Akshay-Product-Cogni <67697970+Akshay-Product-Cogni@users.noreply.github.com>
2023-03-20 19:32:40 +0530

    Added Origin to the Alert Message and Console log

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.addEventListener("message", function (event) {
-    console.log("Received message from iFrame: ", event.data);
-    alert("Received message from iFrame: "+ event.data);
+    console.log("Received message from iFrame: ", event.data, "\nOrigin of the message :", event.origin);
+    alert("Received message from iFrame: "+ event.data + "\nOrigin of the message :" + event.origin);
 
 });
